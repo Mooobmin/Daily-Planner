4da53d39f1e4e29798142dc5227ba37dd5cd3219
Minsub Moon <ics9133@naver.com>
2023-07-28 22:37:15 +0900

    project first commit

---
+++ src/test/java/com/daliyplanner/DaliyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daliyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
